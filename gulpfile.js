8f6abd7912c471ef592bdc97b590212645102713
Byron Spencer <spencer.byron@gmail.com>
2022-06-14 17:21:02 -0500

    More gulp tasks and clean log formatting

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,9 @@ const { src, dest, watch, series, parallel } = require( 'gulp' );
 
 const glob          = require( 'glob' ); // synchronous glob streaming
 const fs            = require( 'fs' ); // file reader
-const permissions   = require( 'gulp-chmod' ); // change file permissions
+const chmod         = require( 'gulp-chmod' ); // change file permissions
 const tap           = require( 'gulp-tap' ); // file contents access
 const axios         = require( 'axios' ); // make http requests
-const gulpChmod = require('gulp-chmod');
-const { isWhileStatement } = require('typescript');
 
 // Default task is required but we don't use it for anything
 exports.default = cb => { cb(); };
@@ -30,62 +28,129 @@ const serverLogSeachPatterns = {
     disconnect: /User '{Steam ([0-9]+)}' disconnected\./g
 }
 
-// parse the server log file
-function parseServerLog() {
-
-    const userTracker = [];
-
-    
-
-    return src( files.cleanLog )
-        .pipe( tap( file => {
-            const newLog = file.contents;
-            console.log( JSON.parse( newLog.toString() ) );
-
-
-            fs.readFile( glob.sync( files.serverLog )[0], {}, ( err, contents ) => {
-                let connection;
-                let disconnection;
-                let uIdx = -1;
-        
-                // check connections first and set initial statuses for each user
-                let re = new RegExp( serverLogSeachPatterns.connect );
-                while( connection = re.exec( contents ) ) {
-                    uIdx = userTracker.findIndex( user => user.steamId === connection[2] );
-        
-                    if( uIdx > -1 ) {
-                        userTracker[ uIdx ].lastSession = connection[1];
-                        userTracker[ uIdx ].lastLogIndex = re.lastIndex;
-                    }
-                    else {
-                        userTracker.push( {
-                            characterName: connection[3],
-                            status: 'online',
-                            characterId: connection[4],
-                            steamId: connection[2],
-                            lastSession: connection[1],
-                            lastLogIndex: re.lastIndex
-                        } );
-                    }
-                }
-        
-                // now we check disconnects to see who is offline
-                re = new RegExp( serverLogSeachPatterns.disconnect );
-                while( disconnection = re.exec( contents ) ) {
-                    uIdx = userTracker.findIndex( user => user.lastSession === disconnection[1] );
-        
-                    if( uIdx > -1 ) {
-                        if( re.lastIndex > userTracker[ uIdx ].lastLogIndex ) {
-                            userTracker[ uIdx ].status = 'offline';
-                        }
-                        userTracker[ uIdx ].lastLogIndex = re.lastIndex;
-                    }
-                }
-        
-                console.log( userTracker );
-            });
-            
-        }));
+function hasJsonStructure( str ) {
+    if (typeof str !== 'string') return false;
+    try {
+        const result = JSON.parse(str);
+        const type = Object.prototype.toString.call(result);
+        return type === '[object Object]' 
+            || type === '[object Array]';
+    } catch (err) {
+        return false;
+    }
 }
 
-exports['parse-server-log'] = parseServerLog;
+function numberfyRegExResults( results ) {
+    return results.map( (r, i) => {
+        if( i !== 3 ) return parseInt( r );
+        return r;
+    } )
+}
+
+function getUserInfoFromServerLog() {
+    const users = [];
+
+    return new Promise( (resolve, reject) => {
+        fs.readFile( glob.sync( files.serverLog )[0], {}, ( err, contents ) => {
+            // connection search
+            let connection;
+            // disconnection search
+            let disconnection;
+            // user index
+            let uIdx = -1;
+            // check connections first and set initial statuses for each user
+            let re = new RegExp( serverLogSeachPatterns.connect );
+
+            if( err ) {
+                reject( err );
+                return;
+            }
+
+            while( connection = re.exec( contents ) ) {
+                connection = numberfyRegExResults( connection );
+                uIdx = users.findIndex( user => user.steamId === connection[2] );
+    
+                if( uIdx > -1 ) {
+                    users[ uIdx ].lastSession = connection[1];
+                    users[ uIdx ].lastLogIndex = re.lastIndex;
+                }
+                else {
+                    users.push( {
+                        characterName: connection[3],
+                        statusOnline: true,
+                        characterId: connection[4],
+                        steamId: connection[2],
+                        lastSession: connection[1],
+                        lastLogIndex: re.lastIndex
+                    } );
+                }
+            }
+    
+            // now we check disconnects to see who is offline
+            re = new RegExp( serverLogSeachPatterns.disconnect );
+            while( disconnection = re.exec( contents ) ) {
+                disconnection = numberfyRegExResults( disconnection );
+                uIdx = users.findIndex( user => user.lastSession === disconnection[1] );
+    
+                if( uIdx > -1 ) {
+                    if( re.lastIndex > users[ uIdx ].lastLogIndex ) {
+                        users[ uIdx ].statusOnline = false;
+                    }
+                    users[ uIdx ].lastLogIndex = re.lastIndex;
+                }
+            }
+
+            resolve( users );
+        });
+    });
+}
+
+function getServerStatusFromConnectionLog() {
+    return new Promise( (resolve, reject) => {
+        console.log( 'getting server status' );
+        resolve( true );
+    });
+}
+
+function updateCleanLog( users ) {
+    return src( files.cleanLog )
+        .pipe( chmod( {
+            read: true,
+            write: true,
+            execute: true
+        }))
+        .pipe( tap( file => {
+            const fileStr = file.contents.toString();
+            const log = hasJsonStructure( fileStr ) ? JSON.parse( fileStr ) : {};
+            const storedUsers = log.users || [];
+
+            storedUsers.forEach( su => {
+                if( !users.find( u => u.characterId === su.characterId ) ) {
+                    su.statusOnline = false;
+                    users.push( su );
+                }
+            })
+
+            log.users = users;
+
+            file.contents = Buffer.from( JSON.stringify( log, null, '\t' ) );
+
+        }))
+        .pipe( dest( './' ) );
+}
+
+function updateTask() {
+
+    let users = [];
+
+    return getUserInfoFromServerLog()
+    .then( u => {
+        users = u;
+        return getServerStatusFromConnectionLog();
+    })
+    .then( s => {
+        return updateCleanLog( users );
+    });
+}
+
+exports['update'] = updateTask;
